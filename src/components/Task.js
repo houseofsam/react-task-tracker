aef422e40a6ed09d343b47ad5218de2063427cce
houseofsam <samxsaid@gmail.com>
2021-12-18 16:02:48 -0500

    create Task.js (component for each separate task). In the return function give it a className of 'task' to match css style selectors. Pass in task as a prop and set <h3> and <p> text. 2) Import Tasks.js and for the map() callback, instead of declaring an <h3>, pass in the Task component instead along with the task prop (html key attribute remains the same). Delete Icon next.

---
+++ src/components/Task.js
@@ -0,0 +1,11 @@
+// A component for each task
+const Task = ({ task }) => {
+  return (
+    <div className='task'>
+      <h3>{task.text}</h3>
+      <p>{task.day}</p>
+    </div>
+  )
+}
+
+export default Task
