6060266d54e062e73bb41f09cd57763eef03bcb3
houseofsam <samxsaid@gmail.com>
2021-12-18 13:46:54 -0500

    Refactor onClick in Button.js to a prop and use that prop when Button is imprted to Header and pass in a function that is defined before the return value when prop is used.

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,14 @@ import PropTypes from 'prop-types'
 
 // Create new/separate button component
 // In case I want to add more buttons, perhaps with diff . colours, texts and events
-const Button = ({ color, text }) => {
-  const onClick = () => {
-    console.log('click');
-  }
-  return <button onClick={onClick} style={{ backgroundColor: color }}className='btn'>{text}</button>
+const Button = ({ color, text, onClick }) => {
+  return <button 
+    onClick={onClick}
+    style={{ backgroundColor: color }}
+    className='btn'
+    >
+      {text}
+    </button>
 }
 
 Button.defaultProps = {
@@ -16,6 +19,7 @@ Button.defaultProps = {
 Button.propTypes = {
   text: PropTypes.string,
   color: PropTypes.string,
+  onClick: PropTypes.func,
 }
 
 export default Button
