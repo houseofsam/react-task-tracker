61473c05975d18078cc8cc82186e36ff3813be37
houseofsam <samxsaid@gmail.com>
2021-12-19 03:29:07 -0500

    v1.3

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const About = () => {
   return (
     <div>
-      <h4>Version 1.1</h4>
+      <h4>Version 1.3</h4>
       <Link to="/">Go Back</Link>
     </div>
   )
