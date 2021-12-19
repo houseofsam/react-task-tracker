4d5e396c809d6e2d1ca5e3f92577f9198799f0ec
houseofsam <samxsaid@gmail.com>
2021-12-19 03:19:00 -0500

    Install react-dom-router v5.2.0. Create Footer and About components. Implement Routes to About page (remove Add Button from header + other components) & Home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import { useState, useEffect } from 'react'
-import Header from './components/Header'
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
+import About from './components/About';
 
 function App() {
   // State for the Add Button which toggles form display. 
@@ -15,7 +18,7 @@ function App() {
     const getTasks = async() => {
       const tasksFromServer = await fetchTasks();
       // add it to our state
-      setTasks(tasksFromServer)
+      setTasks(tasksFromServer);
     }
 
     getTasks();
@@ -25,7 +28,7 @@ function App() {
   // Fetch Tasks
   const fetchTasks = async() => {
     // fetch returns a promise so we want to await that promise
-    const res = await fetch('http://localhost:5000/tasks')
+    const res = await fetch('http://localhost:5000/tasks');
     const data = await res.json();
 
     return data;
@@ -42,7 +45,7 @@ function App() {
       },
       // convert task data being passed into function from object to JSON then send it
       body: JSON.stringify(task)
-    })
+    });
     
     // data that's returned is just new task that's added (as a promise)
     const data = await res.json();
@@ -79,7 +82,7 @@ function App() {
   // Fetch (single) Task for reminder toggle
   const fetchTask = async(id) => {
     // fetch returns a promise so we want to await that promise
-    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
     const data = await res.json();
 
     return data;
@@ -89,7 +92,7 @@ function App() {
   const toggleReminder = async (id) => {
     const taskToToggle = await fetchTask(id);
     // create var for the new (updated) task and put it in a variable BUT change its reminder boolean value
-    const updatedTask = {...taskToToggle, reminder: !taskToToggle.reminder }
+    const updatedTask = {...taskToToggle, reminder: !taskToToggle.reminder };
 
     const res = await fetch(`http://localhost:5000/tasks/${id}`, {
       method: 'PUT',
@@ -97,7 +100,7 @@ function App() {
         'Content-type': 'application/json'
       },
       body: JSON.stringify(updatedTask)
-    })
+    });
 
     // return promise with the updated data app sent to server
     const data = await res.json();
@@ -109,16 +112,28 @@ function App() {
       //*** !task.reminder to data.reminder for server update *** 
       task.id === id ? { ...task, reminder: data.reminder } : task
       // spread the task object, and for reminder, change it to opposite of what it was
-    ))
-  }
+    ));
+  };
 
   return (
-    <div className='container'>
-      <Header toggleForm={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
-      {showAddTask && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'Task list is empty!'}
-    </div>
+    <Router>
+      <div className='container'>
+        <Header toggleForm={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+        <Route 
+          path='/'
+          exact
+          render={(props) => (
+            <>
+            {showAddTask && <AddTask onAdd={addTask} />}
+            {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'Task list is empty!'}
+            </>
+          )} 
+        />
+        <Route path='/about' component={About} />
+        <Footer />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
