1ab104bd516c9984f9db1e2e3729e9449f784318
houseofsam <samxsaid@gmail.com>
2021-12-19 02:25:07 -0500

    Implement delete, add, and toggle reminders to server

--- src/App.js
+++ src/App.js
@@ -31,36 +31,83 @@ function App() {
     return data;
   };
 
+
+  
   // Add task
-  const addTask = (task) => {
+  const addTask = async(task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      // convert task data being passed into function from object to JSON then send it
+      body: JSON.stringify(task)
+    })
+    
+    // data that's returned is just new task that's added (as a promise)
+    const data = await res.json();
+    
+    // add what we just sent to DB to the existing tasks state
+    setTasks([...tasks, data]);
+    
+    /* 
+    Code prior to working with backend
+    
     // Create random ID for each entry
     const id = Math.floor(Math.random() * 10000) + 1;
-
+    
     // create new object and give it the id property we just created
     // and copy in the task object that's passed into the function
     const newTask = { id, ...task };
-
+    
     // call state function, set it as an array, copy the current tasks + and add new task to the end of array
     setTasks([...tasks, newTask])
-    
+    */
   }
-
+  
   // Delete task
-  const deleteTask = (id) => {
-    // console.log('delete', id);
-
+  const deleteTask = async(id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE'
+    });
+    
     // set the tasks to the filtered tasks
     // essentially only include array/list values where task.id does not equal the id being passed in as an argument
     setTasks(tasks.filter((task) => task.id !== id ));
   }
 
+  // Fetch (single) Task for reminder toggle
+  const fetchTask = async(id) => {
+    // fetch returns a promise so we want to await that promise
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json();
+
+    return data;
+  };
+
   // Toggle reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    // create var for the new (updated) task and put it in a variable BUT change its reminder boolean value
+    const updatedTask = {...taskToToggle, reminder: !taskToToggle.reminder }
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updatedTask)
+    })
+
+    // return promise with the updated data app sent to server
+    const data = await res.json();
+
     setTasks(tasks.map((task) => 
       // if task id in the current iteration is equal to the id passed in
       // then we're going to have a specific object (modify that partiular task),
       // else just return the task (no change)
-      task.id === id ? { ...task, reminder: !task.reminder } : task 
+      //*** !task.reminder to data.reminder for server update *** 
+      task.id === id ? { ...task, reminder: data.reminder } : task
       // spread the task object, and for reminder, change it to opposite of what it was
     ))
   }
